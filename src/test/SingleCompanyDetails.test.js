fb71c52a92041b633ccee286479a241102b9c813
MargaretTechworld <margarettechworld@gmail.com>
2023-09-08 00:02:33 +0000

    Add test for SingleCompanyDetails components

---
+++ src/test/SingleCompanyDetails.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import SingleCompanyDetails from '../components/CompanyDetails';
+
+test('Check if SingleCompanyDetails component is rendered correctly', () => {
+  const tree = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <SingleCompanyDetails />
+      </BrowserRouter>
+    </Provider>,
+  );
+
+  expect(tree).toMatchSnapshot();
+});
